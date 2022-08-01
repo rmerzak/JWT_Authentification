a1ccbad6d9878b50de808fec28e3cd25b0fa8fc4
Rabi Merzak <rmerzak@e3r10p11.1337.ma>
2022-08-01 21:43:30 +0100

    finishing the backend

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -6,7 +6,7 @@ const Goal = require('../models/goalModel')
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find()
+  const goals = await Goal.find({user: req.user.id})
 
   res.status(200).json(goals)
 })
@@ -22,6 +22,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id
   })
 
   res.status(200).json(goal)
@@ -38,9 +39,21 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not found')
   }
 
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('user not found')
+  }
+  // Make sure the logged in user matched the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error('Goal not found');
+  }
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   })
+
 
   res.status(200).json(updatedGoal)
 })
@@ -56,6 +69,15 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not found')
   }
 
+  if (!user) {
+    res.status(401);
+    throw new Error('user not found')
+  }
+  // Make sure the logged in user matched the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error('Goal not found');
+  }
   await goal.remove()
 
   res.status(200).json({ id: req.params.id })
